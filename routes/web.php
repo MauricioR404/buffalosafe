1cfb072b1b637e21124f4e679d3f7d94cd3dc5b1
Mauricio <mauweb98@gmail.com>
2019-04-27 21:40:24 -0500

    Complete Homepage

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,12 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
+//Auth-Login-registro-etc
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+//FrontEnd
+Route::view('/', 'index')->name('home');
+Route::view('/seguridad', 'secure')->name('secure');
+
+//Admin
+Route::get('/home', 'HomeController@index')->name('dashboard');
